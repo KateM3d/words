c1d33dc56bd77c5c9e024f7955c6fa98f457b47b
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-11 18:21:27 -0500

    created footer component

---
+++ words/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footerText">All rights reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
