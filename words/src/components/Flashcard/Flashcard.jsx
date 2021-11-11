c5e856bc15bae100fcb2ebfd774b799bccf68205
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-11 18:22:08 -0500

    created flashcard component

---
+++ words/src/components/Flashcard/Flashcard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Flashcard.scss";
+
+function Flashcard(props) {
+  return (
+    <div className="flashcardBody">
+      <p className={`word_category`}>{props.category}</p>
+      <p className={`word_french`}>{props.french}</p>
+      <p className={`word_transcription`}>{props.transcription}</p>
+    </div>
+  );
+}
+
+export default Flashcard;
