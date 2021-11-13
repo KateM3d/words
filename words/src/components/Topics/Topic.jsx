8fb22ce3dfb0252d6faa0f8b5b3dfb5098692d11
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-12 20:19:34 -0500

    new images for categories

---
+++ words/src/components/Topics/Topic.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Flashcard/Flashcard.scss";
+import sport from "./sport.jpg";
+import travel from "./travel.jpg";
+import fun from "./fun.jpg";
+import colors from "./colors.jpg";
+
+function Topic(props) {
+  return (
+    <div className="flashcardBody">
+      <p className={`word_category`}>{props.category}</p>
+      <img className="topicImage" src={props.image} alt={props.category} />
+    </div>
+  );
+}
+
+export default Topic;
