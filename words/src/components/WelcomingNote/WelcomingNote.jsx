43df2373bee96e0e25b6863fedde2d1e23497c8e
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-13 19:20:44 -0500

    new component

---
+++ words/src/components/WelcomingNote/WelcomingNote.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./WelcomingNote.scss";
+import welcome from "./welcome.jpg";
+import arrow from "./arrow.png";
+
+function WelcomingNote() {
+  return (
+    <div className="welcome">
+      <div className="welcomePage">
+        <img src={welcome} alt="welcome" />
+        <div className="welcomeMessage">
+          <h1 className="welcomeMessageText">Welcome to your jorney!</h1>
+          <h3 className="welcomeMessageText">The easy way to learn French</h3>
+          <h3 className="welcomeMessageText">
+            Please choose one of the categories and start learinig right now!
+          </h3>
+        </div>
+      </div>
+      <div>
+        <img className="welcomeArrow" src={arrow} alt="arrow" />
+      </div>
+    </div>
+  );
+}
+
+export default WelcomingNote;
