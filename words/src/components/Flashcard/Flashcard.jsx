0904c6921397d78fcc52376b278eba01bcf8527c
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-20 21:26:56 -0500

    added useState for translation

--- words/src/components/Flashcard/Flashcard.jsx
+++ words/src/components/Flashcard/Flashcard.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import "./Flashcard.scss";
 
 function Flashcard(props) {
+  const [showTranslation, setShowTranslation] = useState(false);
+
+  let handleTranslation = () => {
+    setShowTranslation(!showTranslation);
+  };
+
+  let hideTranslation = () => {
+    setShowTranslation(!showTranslation);
+  };
+
   return (
     <div className="flashcardBody" draggable>
       <p className={`word_category`}>{props.category}</p>
       <p className={`word_french`}>{props.french}</p>
       <p className={`word_transcription`}>{props.transcription}</p>
+      {showTranslation === true ? (
+        <p onClick={hideTranslation} className={`word_french`}>
+          {props.english}
+        </p>
+      ) : (
+        <button
+          className="word_translation translationBtn"
+          onClick={handleTranslation}
+        >
+          Translation
+        </button>
+      )}
     </div>
   );
 }
