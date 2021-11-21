06cdd6e7faeed7e0f8a8ea53f4559c343004ca52
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-20 22:09:42 -0500

    added new DragArea component

---
+++ words/src/components/DropArea/DropArea.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./DropArea.scss";
+
+function DropArea() {
+  return (
+    <div className="containerDropArea">
+      <div className="dropArea"></div>
+      <div className="dropArea"></div>
+    </div>
+  );
+}
+
+export default DropArea;
