5a4ae2f5b9a0c0cf48fc80adf93bb9b901e65e15
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-20 22:09:22 -0500

    added function drag and drop

--- words/src/components/Flashcard/Flashcard.jsx
+++ words/src/components/Flashcard/Flashcard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "./Flashcard.scss";
+import DropArea from "../DropArea/DropArea";
 
 function Flashcard(props) {
   const [showTranslation, setShowTranslation] = useState(false);
@@ -12,25 +13,70 @@ function Flashcard(props) {
   let hideTranslation = () => {
     setShowTranslation(!showTranslation);
   };
+  //drag and drop flashcards
+  let flashcards = document.querySelectorAll(".flashcardBody");
+  let dragItem;
+
+  for (let flashcard of flashcards) {
+    flashcard.addEventListener("dragstart", dragStart);
+    flashcard.addEventListener("dragend", dragEnd);
+  }
+
+  function dragStart() {
+    setTimeout(() => (this.style.display = "none"), 0);
+    return (dragItem = this);
+  }
+
+  function dragEnd() {
+    setTimeout(() => (this.style.display = "block"), 0);
+    return (dragItem = this);
+  }
+
+  let dropAreas = document.querySelectorAll(".dropArea");
+  for (let dropArea of dropAreas) {
+    dropArea.addEventListener("dragover", dragOver);
+    dropArea.addEventListener("dragenter", dragEnter);
+    dropArea.addEventListener("dragleave", dragLeave);
+    dropArea.addEventListener("drop", drop);
+  }
+  console.log(dropAreas);
+  function drop(e) {
+    e.preventDefault();
+    this.append(dragItem);
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter(e) {
+    e.preventDefault();
+  }
+
+  function dragLeave() {
+    this.style.border = "none";
+  }
 
   return (
-    <div className="flashcardBody" draggable>
-      <p className={`word_category`}>{props.category}</p>
-      <p className={`word_french`}>{props.french}</p>
-      <p className={`word_transcription`}>{props.transcription}</p>
-      {showTranslation === true ? (
-        <p onClick={hideTranslation} className={`word_french`}>
-          {props.english}
-        </p>
-      ) : (
-        <button
-          className="word_translation translationBtn"
-          onClick={handleTranslation}
-        >
-          Translation
-        </button>
-      )}
-    </div>
+    <>
+      <div className="flashcardBody" draggable>
+        <p className={`word_category`}>{props.category}</p>
+        <p className={`word_french`}>{props.french}</p>
+        <p className={`word_transcription`}>{props.transcription}</p>
+        {showTranslation === true ? (
+          <p onClick={hideTranslation} className={`word_french`}>
+            {props.english}
+          </p>
+        ) : (
+          <button
+            className="word_translation translationBtn"
+            onClick={handleTranslation}
+          >
+            Translation
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
