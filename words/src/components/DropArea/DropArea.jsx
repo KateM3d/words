7f3f8b4ef89aa4783630c0a1f0464371539ea99b
KateM3d <ekaterina8medvedeva@gmail.com>
2021-11-21 20:03:06 -0500

    changed drag and drop

--- words/src/components/DropArea/DropArea.jsx
+++ words/src/components/DropArea/DropArea.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./DropArea.scss";
+
+const AreaItem = () => {
+  const ref = useRef(null);
+  return (
+    <div
+      ref={ref}
+      className="dropArea"
+      onDragOver={(e) => e.preventDefault()}
+      onDragEnter={(e) => e.preventDefault()}
+      onDragLeave={() => {
+        if (!ref.current) return;
+        ref.current.style.border = "none";
+      }}
+      onDrop={(e) => {
+        const element = document.getElementById(
+          "flashcardBody" + e.dataTransfer.getData("id")
+        );
+        if (element) {
+          ref.current.append(element);
+          element.style.position = "absolute";
+        }
+      }}
+    ></div>
+  );
+};
 
 function DropArea() {
   return (
     <div className="containerDropArea">
-      <div className="dropArea"></div>
-      <div className="dropArea"></div>
+      <AreaItem />
+      <AreaItem />
     </div>
   );
 }
