35b7694eec2f92a9531bbdb1a0dc269a54fe0a04
KateM3d <ekaterina8medvedeva@gmail.com>
2021-12-02 15:21:45 -0500

    created Flashcard Component

--- words/src/components/Flashcard/Flashcard.jsx
+++ words/src/components/Flashcard/Flashcard.jsx
@@ -1,77 +1,78 @@
-import React, { useRef } from "react";
-import { useState } from "react";
+import React from "react";
 import "./Flashcard.scss";
 
-function Flashcard(props) {
-  const ref = useRef(null); // это просто ссылка на элемент карточки, раньше он у тебя назывался dragItem
-  const [showTranslation, setShowTranslation] = useState(false);
-  const [count, setCount] = useState(0);
+const words = [
+  {
+    id: "1",
+    french: "rouge",
+    transcription: "[ ˈruːʒ ]",
+    english: "red",
+    category: "colors",
+  },
+  {
+    id: "2",
+    french: "gris",
+    transcription: "[ ɡʀi ]",
+    english: "grey",
+    category: "colors",
+  },
+  {
+    id: "3",
+    french: "vert",
+    transcription: "[ vɛʀ ]",
+    english: "green",
+    category: "colors",
+  },
+  {
+    id: "4",
+    french: "blanc",
+    transcription: "[ blɑ̃ ]",
+    english: "white",
+    category: "colors",
+  },
+  {
+    id: "5",
+    french: "noir",
+    transcription: "[ nwaʀ ]",
+    english: "black",
+    category: "colors",
+  },
+  {
+    id: "6",
+    french: "jaune",
+    transcription: "[ ʒon ]",
+    english: "yellow",
+    category: "colors",
+  },
+  {
+    id: "7",
+    french: "bleu",
+    transcription: "[ blo͝o ]",
+    english: "blue",
+    category: "colors",
+  },
+];
 
-  const handleTranslation = () => {
-    setShowTranslation(!showTranslation);
-  };
-
-  const hideTranslation = () => {
-    setShowTranslation(!showTranslation);
-  };
-  function handleIncrementClick() {
-    if (count < props.length) setCount(count + 1);
-  }
-  function handleDecrementClick() {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  }
-
-  function dragStart(e) {
-    const element = ref.current;
-    if (!element) return;
-    e.dataTransfer.setData("id", props.english); // при перетаскивании нужно проставить id, чтобы потом в onDrop можно было бы получить перетаскиваемый элемент
-    setTimeout(() => (element.style.display = "none"), 0);
-  }
-
-  function dragEnd(e) {
-    const element = ref.current;
-    if (!element) return;
-    setTimeout(() => (element.style.display = "block"), 0);
-  }
-
+function Card() {
   return (
-    <>
-      <button onClick={handleDecrementClick}>left</button>
-      <div>
-        <div
-          id={"flashcardBody" + props.english} //id карточки нужен для того, чтобы потом в onDrop получить этот элемент и добавить его в dropArea
-          className={"flashcardBody"}
-          draggable
-          onDragStart={dragStart} // листенеры теперь навешиваются здесь
-          onDragEnd={dragEnd}
-          ref={ref}
-        >
-          <p className={`word_category`}>{props.category}</p>
-          <p className={`word_french`}>{props.french}</p>
-          <p className={`word_transcription`}>{props.transcription}</p>
-          {showTranslation === true ? (
-            <p onClick={hideTranslation} className={`word_french`}>
-              {props.english}
-            </p>
-          ) : (
-            <button
-              className="word_translation translationBtn"
-              onClick={handleTranslation}
-            >
-              Translation
-            </button>
-          )}
+    <div className="containerCards">
+      {words.map((word) => (
+        <div className="cardInner" key={word.id}>
+          <p>{word.category}</p>
+          <p>{word.french}</p>
+          <p>{word.transcription}</p>
+          <p>translation</p>
         </div>
-        <div>
-          <h3 className="itemOfItem">
-            {count ? count : ""}/ {props.length}
-          </h3>
-        </div>
-      </div>
-      <button onClick={handleIncrementClick}>Right</button>
-    </>
+      ))}
+    </div>
+  );
+}
+
+function Flashcard() {
+  return (
+    <div className="containerFlashcard">
+      <Card />
+    </div>
   );
 }
 
