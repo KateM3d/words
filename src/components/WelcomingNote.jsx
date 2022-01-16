9da919f60db30f5b55fd673786cf24fc38b3fee1
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-16 17:16:39 -0500

    added new menu option Words

--- src/components/WelcomingNote.jsx
+++ src/components/WelcomingNote.jsx
@@ -21,8 +21,9 @@ export default function WelcomingNote() {
           <h1 className="welcomeMessageText">Welcome to your jorney!</h1>
           <h3 className="welcomeMessageText">The easy way to learn French</h3>
           <h3 className="welcomeMessageText">
-            Please choose one of the categories and start learinig right now!
+            You will learn words from all different categories.
           </h3>
+          <h3 className="welcomeMessageText">Start learning right now!</h3>
         </div>
       </div>
       <div>
